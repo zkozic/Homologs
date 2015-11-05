aba28decd181a39559ab49e93be9997225fc0d6f
zkozic <zrinko.kozic@ed.ac.uk>
2015-11-05 12:31:28 +0000

    Fixed bugs and added input arguments

--- getHomologSequences.py
+++ getHomologSequences.py
@@ -3,26 +3,25 @@ import argparse
 import urllib2
 
 ##### Parsing command line arguments #####
-#parser = argparse.ArgumentParser()
-#parser.add_argument('-g', '--gene', action = 'store', dest = 'gene', help = 'Target gene query. Use Ensembl ID.')
-#parser.add_argument('-o', '--output', action = 'store', dest = 'outfile', help = 'Output file.')
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--gene', action = 'store', dest = 'gene', help = 'Target gene query. Use Ensembl ID.')
+parser.add_argument('-o', '--output', action = 'store', dest = 'outfile', help = 'Output file.')
+args = parser.parse_args()
 
-gene = 'ENSMUSG00000027168'
-outfile = 'homologs.fa'
+gene = args.gene
+outfile = args.outfile
 
 ##### Get XML over Ensembl REST API
 gene_api = 'http://rest.ensembl.org/homology/id/'+gene+'?content-type=text/xml;type=orthologues;aligned=0;sequence=cdna'
-gene_xml = urllib2.urlopen(geneAPI).read()
-xmldoc = minidom.parseString(data)
+gene_xml = urllib2.urlopen(gene_api).read()
+xmldoc = minidom.parseString(gene_xml)
 
 ##### Parse XML and write FASTA
 homologs = xmldoc.getElementsByTagName('target')
-seq_dict = {}
 f = open(outfile, 'w')
 for i in range(0, len(homologs) - 1):
-    seq_id = '>'str(homologs[i].attributes['species'].value+'_'+homologs[i].attributes['id'].value)
+    seq_id = '>'+str(homologs[i].attributes['species'].value+'_'+homologs[i].attributes['id'].value)
     sequence = str(homologs[i].attributes['seq'].value)
-    f.write(seq_id)
-    f.write(sequence)
+    f.write(seq_id+'\n')
+    f.write(sequence+'\n')
 f.close()
